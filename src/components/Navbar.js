0140341086cb3e51b887aeacaa3fc4775b24ab7e
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-09-02 21:58:51 +0700

    finish Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,28 @@ import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-2 sticky top-0 z-100 h-81 bg-white">
+    <div
+      className="flex justify-between items-center px-10 md:px-5 py-5 sticky top-0 z-100 
+     bg-white lg:px-5 shadow-md"
+    >
       {/* Left */}
       <Link className="flex items-center no-underline" to="/">
         <img className="text-black" src={logo} alt="" srcset="" />
-        <h2>TinCNC</h2>
+        <p className=" text-2xl">TinCNC</p>
       </Link>
 
       {/* Right */}
-      <div className="flex">
+      <div className="flex items-center space-x-2 lg:space-x-20">
         <NavbarItem title="Home" to={"/home"} />
+        <NavbarItem title="Services" to={"/services"} />
+        <NavbarItem title="About" to={"/about"} />
+        <NavbarItem title="Contact" to={"/contact"} />
+        <div
+          className="text-xl font-bold text-blue-500 p-2 cursor-pointer 
+          border-2 border-blue-500 px-4 hover:bg-gray-100"
+        >
+          <p>Login</p>
+        </div>
       </div>
     </div>
   );
--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 const NavbarItem = ({ title, to }) => {
   return (
-    <Link className="no-underline" to={to}>
-      <p>{title}</p>
-    </Link>
+    <div
+      className="flex items-center px-4 rounded-lg h-10 hover:bg-blue-500
+    hover:text-white"
+    >
+      <Link className="text-xl no-underline" to={to}>
+        <p>{title}</p>
+      </Link>
+    </div>
   );
 };
 
