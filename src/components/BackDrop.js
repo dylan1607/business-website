16a50327ebd567a8fd3d6ba9596fa3e3fb80c3b0
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-09-02 22:41:56 +0700

    add backdrop

--- src/components/BackDrop.js
+++ src/components/BackDrop.js
@@ -1,9 +1,13 @@
-const BackDrop = () => {
-    return (
-        <div>
-            
-        </div>
+const BackDrop = ({ show, click }) => {
+  return (
+    show && (
+      <div
+        className="w-full h-screen z-1000 fixed
+        top-0 left-0 backdrop-filter backdrop-blur-sm"
+        onClick={click}
+      ></div>
     )
-}
+  );
+};
 
-export default BackDrop
+export default BackDrop;
