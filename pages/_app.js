ee339d0df14b63237190439393692003296df134
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-10-01 11:20:38 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,18 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Component {...pageProps} />
-      <MessengerCustomerChat pageId="671656946378930" appId="651615732489589" />
-    </div>
+    <>
+      {/* app won't render anything unless the window object is defined; instead the server just returns null */}
+      {typeof window === "undefined" ? null : (
+        <div>
+          <Component {...pageProps} />
+          <MessengerCustomerChat
+            pageId="671656946378930"
+            appId="651615732489589"
+          />
+        </div>
+      )}
+    </>
   );
 }
 
