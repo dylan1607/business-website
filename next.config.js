3611d30ddae21df7a1b739b431d5bc04673fe2d3
dylan <dylan@dylans-MacBook-Pro.local>
2021-10-02 16:12:42 +0700

    add domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com"],
+    domains: [
+      "images.unsplash.com",
+      "public-strapi-cms.s3.ap-southeast-1.amazonaws.com",
+    ],
   },
 };
